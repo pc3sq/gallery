0e824bb9265b807de7a19446f1c3c7387d2ebbd9
Peter Ching <peter.ching@gmail.com>
2013-02-12 15:55:06 -0500

    Initial commit for gallery app based on One Month Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '96fc2153935cf3104ecdd1c03ab82ba06d806e76fdc56a879169567ca2db05e8c86262ee5f8c3b02a42f9710bc403cf50a430faa22a1cc31d0de74255a0d1541'
